dac62afb97b6e3e32c1fe9ee74d68d79915cb913
Marcelo vqvd <marcelovqvd@gmail.com>
2019-04-17 16:24:36 -0300

    improving

--- modal mouseout/script.js
+++ modal mouseout/script.js
@@ -1,20 +1,21 @@
 function iniciaModal(modalID) {
   const modal = document.getElementById(modalID);
-  if (modal) {
-    modal.classList.add('mostrar');
-    modal.addEventListener('mouseout', (e) => {
-      console.log(e);
-      if (e.target == modalID || e.target.className == 'fechar') {
-        modal.classList.remove('mostrar');
-        localStorage.fechaModal = modalID; //Se, não abre mais o modal p o usario
-      }
-    });
+  if (localStorage.fechaModal !== modalID) {
+    if (modal) {
+      modal.classList.add('mostrar');
+      modal.addEventListener('click', (e) => {
+        if (e.target == modalID || e.target.className == 'fechar' || e.target.className == 'button') {
+          modal.classList.remove('mostrar');
+          localStorage.fechaModal = modalID;
+        }
+      });
+    }
   }
 }
 
 const saida = document.querySelector('html');
 
-saida.addEventListener('mouseout', () => iniciaModal('modal-principal'));
+saida.addEventListener('mouseleave', () => iniciaModal('modal-principal'));
 
 /*mouseout ou mouseleave
 Basicamente, unica diferença entre os dois é que, o mouseleave é disparado também nos elementos filhos do elemento selecionado, e o mouseout dispara apenas no elemento selecionado.*/
