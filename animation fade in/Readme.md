360fb42e9ef76abf10807edb0737d4690a841864
Marcelo vqvd <marcelovqvd@gmail.com>
2019-04-23 23:36:22 -0300

    first commit

---
+++ animation fade in/Readme.md
@@ -0,0 +1,6 @@
+<h1>Animation Fade in</h1>
+
+<h2>taken from:</h2>
+<a href="https://daneden.github.io/animate.css/">Daniel Eden github</a>
+<a href="https://daneden.me/">Profile page</a>
+  
---
+++ animation fade in/js/main.js
@@ -0,0 +1,24 @@
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".box1 p").addClass("animated fadeInDown");
+	}, 400);
+});
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".box2 p").addClass("animated fadeInDown");
+	}, 1200);
+});
+
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".box3 p").addClass("animated fadeInDown");
+	}, 1600);
+});
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".inspiration").addClass("animated rollIn");
+	}, 200);
+});
